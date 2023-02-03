c46444f7a48639a9c32aa4ed0c1d5791df92d735
Santiago Pereira <santiagopb@msn.com>
2023-02-03 19:24:35 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Santiago Pereira on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
